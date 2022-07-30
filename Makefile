7e04a1ddda793beb842df270b3d4c252ea64158b
Sotatek-Hai-Pham <hai.pham@sotatek.com>
2022-07-30 20:34:40 +0700

    config for https

--- Makefile
+++ Makefile
@@ -11,3 +11,18 @@ flushredis:
 
 ssh:
 	docker exec -it ghtk_php zsh
+
+set-https:
+	bin/magento setup:store-config:set --base-url="http://" --base-url-secure="https://" --use-secure=1
+
+deploy_14:
+	rsync -avhzL --delete \
+				--no-perms --no-owner --no-group \
+				--exclude .git \
+				--exclude .env \
+				--exclude dist \
+				--exclude tmp \
+				--exclude node_modules \
+				--exclude workers \
+				--filter=":- .gitignore" \
+				/home/sam/Documents/tmdt/TMDT-K63 hai@20.70.76.187:/home/hai/
--- php/start.sh
+++ php/start.sh
@@ -6,7 +6,7 @@ find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {
 composer update;
 composer install;
 
-php -dmemory_limit=4G bin/magento setup:install --base-url=http://localhost --db-host=db --db-name=magentodb --db-user=root --db-password=123456 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@gmail.com --admin-user=admin --admin-password=Admin123456 --language=en_US --currency=USD
+php -dmemory_limit=4G bin/magento setup:install --base-url=http://20.70.76.187 --db-host=db --db-name=magentodb --db-user=root --db-password=123456 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@gmail.com --admin-user=admin --admin-password=Admin123456 --language=en_US --currency=USD
 php-fpm;
 
 chmod -R 777 /magento/var/cache;
